6cc153abe3b747f5db30329dd3fed18dea5484a4
Adrian Althaus <adrian.althaus@stud.hslu.ch>
2018-03-12 21:28:26 +0100

    links page update

--- links.php
+++ links.php
@@ -5,16 +5,51 @@ include ("incl/header.php");
 ?>
 <h1>Links</h1>
 
-<h3>PluSport</h3>
-<a href="https://www.plusport.ch">https://www.plusport.ch</a>
+<table class="uk-table uk-table-divider app_table">
+	<tbody>
+		<tr>
+			<td>
+				<h4>PluSport</h4> <a href="https://www.plusport.ch">https://www.plusport.ch</a>
+			</td>
+			<td>PluSport ist die Fachstelle für den Behindertensport in der
+				Schweiz. Als Dachverband fördert PluSport Menschen mit Behinderung
+				vom Breiten- bis hin zum Spitzensport. Für alle Zielgruppen,
+				Altersklassen, Behinderungsformen, in unterschiedlichen Sportarten
+				und stets mit dem Ziel der Integration und Inklusion.</td>
+			<td class="logo_container"><a href="https://www.plusport.ch"><img
+					src="graphics/logo_plusport.jpg" class="links_logo" /></a></td>
+		</tr>
 
-<h3>Schweizer Paraplegiker Vereinigung</h3>
-<a href="http://www.spv.ch">http://www.spv.ch</a>
+		<tr>
+			<td><h4>Schweizer Paraplegiker Vereinigung</h4> <br /> <a
+				href="http://www.spv.ch">http://www.spv.ch</a></td>
+			<td>Die Schweizer Paraplegiker-Vereinigung ist der nationale
+				Dachverband der Querschnittgelähmten. Wir vereinigen mehr als 11'000
+				Mitglieder. 27 Rollstuhlclubs tragen als regionale Pfeiler den
+				Solidaritätsgedanken in die gesamte Schweiz. Rund 220 Mitarbeitende
+				(davon 100 fest angestellt) sowie 1'800 Freiwillige und
+				Ehrenamtliche ermöglichen ein breites Dienstleistungsangebot</td>
+			<td class="logo_container"><a href="http://www.spv.ch"><img
+					src="graphics/logo-spv.jpg" class="links_logo" /></a></td>
+		</tr>
 
-<h3>Swiss Paralympic</h3>
-<a href="http://www.swissparalympic.ch/">http://www.swissparalympic.ch/</a>
-
-
+		<tr>
+			<td>
+				<h4>Swiss Paralympic</h4> <a href="http://www.swissparalympic.ch/">http://www.swissparalympic.ch/</a>
+			</td>
+			<td>Swiss Paralympic selektioniert die Schweizer
+				Behindertenspitzensportler und -sportlerinnen für Paralympics, Welt-
+				und Europameisterschaften. Dazu gehören auch die Finanzierung und
+				Organisation dieser Teilnahmen. Als nationales Komitee ist die
+				Organisation Mitglied und Ansprechpartnerin des Internationalen
+				Paralympischen Komitees (IPC) in Bonn, welches das Pendant zum
+				Internationalen Olympischen Komitee (IOC) ist.</td>
+			<td class="logo_container"><a href="http://www.swissparalympic.ch/"><img
+					src="graphics/logo_swissparalympics.jpg" class="links_logo" /></a>
+			</td>
+		</tr>
+	</tbody>
+</table>
 
 
 <?php
